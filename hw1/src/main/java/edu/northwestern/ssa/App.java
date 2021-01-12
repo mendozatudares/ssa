c000f7925958ceec406766a22b1a9188a3a66c12
Kevin Mendoza Tudares <mendozatudares@u.northwestern.edu>
2021-01-12 13:50:29 -0600

    no longer requires common crawl filename variable

--- hw1/src/main/java/edu/northwestern/ssa/App.java
+++ hw1/src/main/java/edu/northwestern/ssa/App.java
@@ -11,26 +11,48 @@ import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
         String commonCrawlFilename = System.getenv("COMMON_CRAWL_FILENAME");
-
         S3Client s3 = S3Client.builder()
                 .region(Region.US_EAST_1)
                 .overrideConfiguration(ClientOverrideConfiguration.builder()
                         .apiCallAttemptTimeout(Duration.ofMinutes(30)).build())
                 .build();
+
+        if (commonCrawlFilename == null || commonCrawlFilename.equals("")) {
+            // No specified common crawl file to download. Download the latest file
+            LocalDate date = LocalDate.now();
+            ListObjectsRequest listObjects = getListObjectsRequest(date.getYear(), date.getMonthValue());
+            ListObjectsResponse res = s3.listObjects(listObjects);
+
+            int i = 3;
+            while (res.contents().size() == 0 && i-- > 0) {
+                date = date.minusMonths(1);
+                listObjects = getListObjectsRequest(date.getYear(), date.getMonthValue());
+                res = s3.listObjects(listObjects);
+            }
+
+            List<S3Object> s3Objects = res.contents().stream()
+                    .sorted(Comparator.comparing(S3Object::lastModified))
+                    .collect(Collectors.toList());
+
+            commonCrawlFilename = s3Objects.get(s3Objects.size() - 1).key();
+        }
+
         GetObjectRequest request = GetObjectRequest.builder()
-                .bucket("commoncrawl")
-                .key(commonCrawlFilename)
-                .build();
+            .bucket("commoncrawl")
+            .key(commonCrawlFilename)
+            .build();
         ResponseInputStream<GetObjectResponse> ris = s3.getObject(request);
 
         try {
@@ -39,7 +61,6 @@ public class App {
             ElasticSearch es = new ElasticSearch();
             es.CreateIndex();
 
-            ArrayList<String> docs = new ArrayList<>();
             for (ArchiveRecord r : ar) {
                 String url = r.getHeader().getUrl();
                 if (url == null) continue;
@@ -66,6 +87,14 @@ public class App {
 
         } catch (IOException e) {
             e.printStackTrace();
+            s3.close();
         }
+    }
+
+    private static ListObjectsRequest getListObjectsRequest(int year, int month) {
+        return ListObjectsRequest.builder()
+                .bucket("commoncrawl")
+                .prefix(String.format("crawl-data/CC-NEWS/%04d/%02d/", year, month))
+                .build();
     }
 }
